f2dbc0e37ccd2bdf385706b76f8c05c879e35022
ac2393921 <ac2393921@gmail.com>
2024-06-17 00:03:21 +0900

    add: template

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
 
@@ -17,7 +32,18 @@ async fn main() -> Result<(), actix_web::Error> {
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello, world!";
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
 
     Ok(HttpResponse::Ok().body(response_body))
 }
