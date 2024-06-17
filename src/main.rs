97b968e08fb02a51467ef9e39a1e876bd13792f3
ac2393921 <ac2393921@gmail.com>
2024-06-17 22:26:12 +0900

    feat: db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
@@ -18,18 +18,47 @@ struct IndexTemplate {
 
 #[derive(Error, Debug)]
 enum MyError {
-    #[error("failed to render HTML")]
-    AskamaError(#[from] askama::Error),
+    #[error("Failed to render HTML")]
+    AsakamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
-#[actix_web::main]
-async fn main() -> Result<(), actix_web::Error> {
-    let manager = SqliteConnectionManager::file("tidi.db");
-    let pool = Pool::new(manager).expect("Failed to create pool");
-    let conn = pool.get().expect("Failed to get connection from pool");
+#[get("/")]
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
 
+    let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?);
+    }
+
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
     conn.execute(
         "CREATE TABLE IF NOT EXISTS todo (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,29 +66,16 @@ async fn main() -> Result<(), actix_web::Error> {
         )",
         params![],
     )
-    .expect("Failed to create table");
+    .expect("Failed to create a table `todo.");
 
-    HttpServer::new(move || App::new().service(index).app_data(pool.clone()))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
+
     Ok(())
-}
-
-#[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
-    let mut entries = Vec::new();
-    entries.push(TodoEntry {
-        id: 1,
-        text: "First entry".to_string(),
-    });
-    entries.push(TodoEntry {
-        id: 2,
-        text: "Second entry".to_string(),
-    });
-
-    let html = IndexTemplate { entries };
-    let response_body = html.render()?;
-
-    Ok(HttpResponse::Ok().body(response_body))
 }
